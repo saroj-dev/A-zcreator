4cc376e28e1a8bb60c54c700117401ab266af7be
Rajesh_Khadka <rajeshkhadkaofficial45@gmail.com>
2021-09-04 18:19:15 +0545

    Another Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import Nav from "./Components/Nav/Nav";
 import "./Styles/GlobalCSS/GlobalCSS.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 const App = () => {
   return (
     <>
       <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </>
   );
 };
---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div>Hey I'm homepage'</div>;
+};
+
+export default HomePage;
